a8eacd667a5c434832425ecfcb014416080544d9
Mateus Costa <mateus_c@dell.com>
2023-10-19 22:31:05 -0300

    Added the correct dependency towork with persistency byt using the jakarta and spring-boot-starer-data-jpa

--- src/main/java/myspringapp/springtwitter/entities/TweetEntity.java
+++ src/main/java/myspringapp/springtwitter/entities/TweetEntity.java
@@ -1,13 +1,26 @@
 package myspringapp.springtwitter.entities;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
+import myspringapp.springtwitter.dto.CommentDTO;
 
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entitity
+@Entity
+@Table(name = "tweet")
 public class TweetEntity {
+    @Id
+    private String id;
+    // TODO colocar entity do user
+//    private UserDTO user;
+    private String value;
+    private String date;
+    private int likes;
+    // TODO colocar entity do comment
+    private CommentDTO[] comments;
 }
--- src/main/java/myspringapp/springtwitter/repositories/TweetRepository.java
+++ src/main/java/myspringapp/springtwitter/repositories/TweetRepository.java
@@ -1,7 +1,11 @@
 package myspringapp.springtwitter.repositories;
 
+import myspringapp.springtwitter.entities.TweetEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TweetRepository extends CrudRepository<TweetDTO, String>{
+public interface TweetRepository extends CrudRepository<TweetEntity, String> {
+    Page<TweetEntity> findAllById(Long id);
 }
